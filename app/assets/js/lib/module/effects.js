88042d4325fd8bc86b90fa7fa79296a661c57c9f
DND-dev <frontend.work@icloud.com>
2022-07-20 19:56:53 +0300

    add methods heightIn and heightOut for effects, add property autoValue

--- app/assets/js/lib/module/effects.js
+++ app/assets/js/lib/module/effects.js
@@ -30,8 +30,16 @@ dnd.prototype.animateStart = function ({ duration, animateAction, maxValue, fina
     return _animationMake
 }
 
-dnd.prototype.applyAnimateIn = function ({ duration, display, finallyFunction, styleProperty, maxValue = 1, units = "" }) {
+dnd.prototype.applyAnimateIn = function ({ duration, display, finallyFunction, styleProperty, maxValue = 1, units = "", autoValue = false }) {
     const _applyAnimate = (elem) => {
+        let triggerStart = true
+        if (autoValue && triggerStart) {
+            elem.style.display = display
+            maxValue = +window.getComputedStyle(elem)[styleProperty].replace(/px/, "")
+            units = "px"
+            triggerStart = false
+            elem.style.display = "none"
+        }
         const _animateIn = (stepAnimate) => {
                 elem.style.display = display
                 elem.style[styleProperty] = `${stepAnimate}${units}`
@@ -55,8 +63,15 @@ dnd.prototype.applyAnimateIn = function ({ duration, display, finallyFunction, s
     }
 }
 
-dnd.prototype.applyAnimateOut = function ({ duration, finallyFunction, styleProperty, maxValue = 1, units = "" }) {
+dnd.prototype.applyAnimateOut = function ({ duration, finallyFunction, styleProperty, maxValue = 1, units = "", autoValue = false }) {
     const _applyAnimate = (elem) => {
+        let triggerStart = true
+        if (autoValue && triggerStart) {
+            maxValue = +window.getComputedStyle(elem)[styleProperty].replace(/px/, "")
+            units = "px"
+            triggerStart = false
+        }
+
         const _animateOut = (stepAnimate) => {
                 elem.style[styleProperty] = `${maxValue - stepAnimate}${units}`
 
@@ -84,41 +99,49 @@ dnd.prototype.applyAnimateOut = function ({ duration, finallyFunction, styleProp
 }
 // !Технические методы end
 
-dnd.prototype.fadeIn = function ({ duration, display = "block", finallyFunction }) {
+dnd.prototype.fadeIn = function ({ duration, display = "block", finallyFunction, maxValue = 1, units = "" }) {
     this.applyAnimateIn({
         duration,
         display,
         finallyFunction,
-        styleProperty: "opacity"
+        styleProperty: "opacity",
+        maxValue,
+        units
     })
 
     return this
 }
 
-dnd.prototype.fadeOut = function ({ duration, finallyFunction }) {
+dnd.prototype.fadeOut = function ({ duration, finallyFunction, maxValue = 1, units = "" }) {
     this.applyAnimateOut({
         duration,
         finallyFunction,
-        styleProperty: "opacity"
+        styleProperty: "opacity",
+        maxValue,
+        units
     })
 
     return this
 }
 
-dnd.prototype.fadeToogle = function ({ duration, display = "block", finallyFunction }) {
+dnd.prototype.fadeToogle = function ({ duration, display = "block", finallyFunction, maxValue = 1, units = "" }) {
     const _selectAnimate = (elem) => {
         if (window.getComputedStyle(elem).display === "none") {
             this.applyAnimateIn({
                 duration,
                 display,
                 finallyFunction,
-                styleProperty: "opacity"
+                styleProperty: "opacity",
+                maxValue,
+                units
             })
         } else {
             this.applyAnimateOut({
                 duration,
                 finallyFunction,
-                styleProperty: "opacity"
+                styleProperty: "opacity",
+                maxValue,
+                units
             })
         }
     }
@@ -132,29 +155,60 @@ dnd.prototype.fadeToogle = function ({ duration, display = "block", finallyFunct
 }
 // !прозрачность end
 
-dnd.prototype.widthIn = function ({ duration, display = "block", maxValue = 100, units = "", finallyFunction }) {
+dnd.prototype.widthIn = function ({ duration, display = "block", maxValue = 100, units = "", finallyFunction, autoValue = false }) {
     this.applyAnimateIn({
         duration,
         display,
         finallyFunction,
         styleProperty: "width",
         maxValue,
-        units
+        units,
+        autoValue
     })
 
     return this
 }
 
-dnd.prototype.widthOut = function ({ duration, maxValue = 100, units = "", finallyFunction }) {
+dnd.prototype.widthOut = function ({ duration, maxValue = 100, units = "", finallyFunction, autoValue = false }) {
     this.applyAnimateOut({
         duration,
         finallyFunction,
         styleProperty: "width",
         maxValue,
-        units
+        units,
+        autoValue
     })
 
     return this
 }
 
 // !Ширина end
+
+dnd.prototype.heightIn = function ({ duration, display = "block", maxValue = 100, units = "", finallyFunction, autoValue = false }) {
+    this.applyAnimateIn({
+        duration,
+        display,
+        finallyFunction,
+        styleProperty: "height",
+        maxValue,
+        units,
+        autoValue
+    })
+
+    return this
+}
+
+dnd.prototype.heightOut = function ({ duration, maxValue = 100, units = "", finallyFunction, autoValue = false }) {
+    this.applyAnimateOut({
+        duration,
+        finallyFunction,
+        styleProperty: "height",
+        maxValue,
+        units,
+        autoValue
+    })
+
+    return this
+}
+
+// !Высота end
